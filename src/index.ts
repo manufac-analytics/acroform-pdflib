fecfd07c3ccf1f87226022dd05bdb54635c7a3c7
maneetgoyal <mgoyal@manufacanalytics.com>
2025-09-21 19:57:42 +0530

    Add PDF form flattening functionality and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("This is a template for node.js library");
+import { PDFDocument } from "pdf-lib";
+
+async function flattenForm() {
+  const formUrl = "https://pdf-lib.js.org/assets/form_to_flatten.pdf";
+  const formPdfBytes = await fetch(formUrl).then((res) => {
+    return res.arrayBuffer();
+  });
+
+  const pdfDoc = await PDFDocument.load(formPdfBytes);
+
+  const form = pdfDoc.getForm();
+
+  form.getTextField("Text1").setText("Some Text");
+  form.getRadioGroup("Group2").select("Choice1");
+  form.getRadioGroup("Group3").select("Choice3");
+  form.getRadioGroup("Group4").select("Choice1");
+  form.getCheckBox("Check Box3").check();
+  form.getCheckBox("Check Box4").uncheck();
+  form.getDropdown("Dropdown7").select("Infinity");
+  form.getOptionList("List Box6").select("Honda");
+
+  form.flatten();
+
+  await pdfDoc.save();
+}
+
+flattenForm().catch((err: unknown) => {
+  console.log(err);
+});
